7df3691538d4f77f8f9ff61262ce4759565bcd2d
John Baber <git@frundle.com>
2018-01-08 12:01:47 +0000

    Use espeak

--- listener.sh
+++ listener.sh
@@ -6,7 +6,7 @@ while true; do
   read -r -s -t 0.5; RETVAL=$?
   [ $RETVAL -eq 0 ] && echo -E "$REPLY" && SOFAR="$SOFAR"$'\n'"$REPLY" && continue
   if [ "$SOFAR" ]; then
-    echo "$SOFAR" |text2wave |lame --quiet - "$(printf "%03d.mp3" $COUNTER)"
+    echo "$SOFAR" |espeak --stdin -s 175 -k20 --stdout |lame --quiet - "$(printf "%03d.mp3" $COUNTER)"
     COUNTER=$(($COUNTER + 1))
     SOFAR=""
   fi
