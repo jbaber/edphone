a70d7b2bdfbd43b41080b9840bf4ec22caf03004
John Baber <git@frundle.com>
2018-01-08 04:28:40 +0000

    Use bash

---
+++ listener.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+COUNTER=1
+SOFAR=""
+while true; do
+  read -r -s -t 0.5; RETVAL=$?
+  [ $RETVAL -eq 0 ] && echo -E "$REPLY" && SOFAR="$SOFAR"$'\n'"$REPLY" && continue
+  if [ "$SOFAR" ]; then
+    echo "$SOFAR" > $(printf "%03d.mp3" $COUNTER)
+    COUNTER=$(($COUNTER + 1))
+    SOFAR=""
+  fi
+  # no timeout ? (EOF or error) break
+  [ $RETVAL -gt 128 ] || break
+done
