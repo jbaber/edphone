4aabc81a5d133b8189f13b1dd85bd3bb9e956eb2
John Baber <git@frundle.com>
2018-01-08 04:57:55 +0000

    Actually create mp3s

--- listener.sh
+++ listener.sh
@@ -6,7 +6,7 @@ while true; do
   read -r -s -t 0.5; RETVAL=$?
   [ $RETVAL -eq 0 ] && echo -E "$REPLY" && SOFAR="$SOFAR"$'\n'"$REPLY" && continue
   if [ "$SOFAR" ]; then
-    echo "$SOFAR" > $(printf "%03d.txt" $COUNTER)
+    echo "$SOFAR" |text2wave |lame - "$(printf "%03d.mp3" $COUNTER)"
     COUNTER=$(($COUNTER + 1))
     SOFAR=""
   fi
